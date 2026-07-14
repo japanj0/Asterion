aaf2e869354467190fb8e44805251c5293b3a600
japanj0 <socolov9066@gmail.com>
2026-07-14 22:34:30 +0300

    исправленна критическая ошибка связанная с импортом библиотек

--- src/admin/main.py
+++ src/admin/main.py
@@ -6,17 +6,11 @@ import sqlite3
 import threading
 import socket
 import hashlib
-import base64
 from datetime import datetime
-from io import BytesIO
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from crypto import CryptoManager
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
 
 os.makedirs("admin", exist_ok=True)
 os.makedirs("database", exist_ok=True)
@@ -42,6 +36,12 @@ def hash_password(password):
 
 
 def generate_self_signed_cert():
+    from cryptography import x509
+    from cryptography.x509.oid import NameOID
+    from cryptography.hazmat.primitives import hashes, serialization
+    from cryptography.hazmat.primitives.asymmetric import rsa
+    import datetime
+
     key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     subject = issuer = x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, "RU"),
@@ -51,8 +51,9 @@ def generate_self_signed_cert():
         x509.NameAttribute(NameOID.COMMON_NAME, "localhost"),
     ])
     cert = x509.CertificateBuilder().subject_name(subject).issuer_name(issuer).public_key(
-        key.public_key()).serial_number(x509.random_serial_number()).not_valid_before(datetime.now()).not_valid_after(
-        datetime.now() + datetime.timedelta(days=3650)).add_extension(
+        key.public_key()).serial_number(x509.random_serial_number()).not_valid_before(
+        datetime.datetime.now(datetime.UTC)).not_valid_after(
+        datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=3650)).add_extension(
         x509.SubjectAlternativeName([x509.DNSName("localhost")]), critical=False).sign(key, hashes.SHA256())
 
     with open("admin/server.crt", "wb") as f:
@@ -699,6 +700,9 @@ class MainWindow(QMainWindow):
         print(f"Получен скриншот от {username}, длина данных: {len(screen_data)}")
         if self.current_screen_user == username:
             try:
+                from io import BytesIO
+                import base64
+                from PyQt6.QtGui import QPixmap
                 img_bytes = base64.b64decode(screen_data)
                 pixmap = QPixmap()
                 pixmap.loadFromData(img_bytes, "JPEG")
